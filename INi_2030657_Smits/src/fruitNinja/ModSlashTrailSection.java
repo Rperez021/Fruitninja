cf156b95e082426c437735b2a17ec10c47da56ee
Gertjan Smits <gertjan_smits@hotmail.com>
2015-07-02 08:16:31 +0200

    Final push
    De laatste functies werken nu

--- INi_2030657_Smits/src/fruitNinja/ModSlashTrailSection.java
+++
@@ -1,5 +0,0 @@
-package fruitNinja;
-
-public class ModSlashTrailSection {
-
-}
--- ViewPlayingFieldPanel.java
+++ ViewPlayingFieldPanel.java
@@ -1,68 +1,62 @@
 package fruitNinja;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class ViewPlayingFieldPanel extends JPanel implements Runnable{
+@SuppressWarnings("serial")
+public class ViewPlayingFieldPanel extends JPanel implements ActionListener{
 	
-	private ConMouseController c_MouseController;
-	private ModSlashTrailSection slash;
+	private Timer timer = new Timer(1, this);
 	private ModPlayingField m_PlayingField;
-	private Font f = new Font("SansSerif", Font.BOLD, 20);
 	private Image img;
 	private Image fruit;
-	private int x;
-	private int y;
+	private ConMouseController mouse;
+	private ModSlashTrailSection section;
 
-	public ViewPlayingFieldPanel(ModPlayingField m_PlayingField) {
+	public ViewPlayingFieldPanel(ModPlayingField m_PlayingField, ConMouseController mouse, ModSlashTrailSection section) {
 		this.m_PlayingField = m_PlayingField;
-		this.c_MouseController = new ConMouseController();
-		this.slash = new ModSlashTrailSection(c_MouseController, m_PlayingField);
-		this.setPreferredSize(new Dimension(500, 500));
+		this.mouse = mouse;
+		this.section = section;
+		this.addMouseListener(mouse);
+		m_PlayingField.throwObject();
 		try {
 			this.img = ImageIO.read(ClassLoader.getSystemResource("images/background.png"));
 		} catch (IOException e) {
 			System.out.println("kan image niet vinden");
 		}
-		this.run();
+		this.setPreferredSize(new Dimension(500, 500));
+		timer.start();
 	}
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.drawImage(img, 0, 0, null);
-		if(m_PlayingField.getThrown() == false){
-			m_PlayingField.getObject().fillArray();
-			m_PlayingField.throwObject();
-			try {
-				 this.fruit = ImageIO.read(ClassLoader.getSystemResource("images/" + m_PlayingField.getObject().getSoort() + ".png"));
-			} catch (IOException e) {
-				System.out.println("kan image niet vinden");
-			}
-			g.drawImage(fruit, this.x, this.y, null);
-			m_PlayingField.setThrown(false);
-		}	
-		
-	}
-
-	@Override
-	public void run() {
-		while(true){
-			this.x = m_PlayingField.getObject().getX();
-			this.y = m_PlayingField.getObject().getY();
-			this.repaint();
+		try {
+			this.fruit = ImageIO.read(ClassLoader.getSystemResource("images/" + m_PlayingField.getObject().getSoort() + ".png"));
+		} catch (IOException e) {
+			System.out.println("kan image niet vinden");
 		}
+		g.drawImage(fruit, this.m_PlayingField.getObject().getX(), this.m_PlayingField.getObject().getY(), null);
+	}	
+		
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(!this.section.isGameover()){
+			if(e.getSource()==timer){
+				this.section.checkhit();
+				this.m_PlayingField.update();
+				this.repaint();
+			}
+		}
+		
 	}
 
 }
